73895412a903fdd5d51d7d15ccd18b1251cba426
likhitawoona <likhitawoona@gmail.com>
2025-07-07 18:25:43 +0530

    variablesnotes

---
+++ pract.py
@@ -0,0 +1,20 @@
+arai_mileage = 19.1
+print(type(arai_mileage))
+
+engine_displacement = 1493 
+print(type(engine_displacement))
+
+max_Power = 114
+print(type(max_Power))
+
+seatingcapacity=5
+print(type(seatingcapacity))
+
+fuel_type="diesel"
+print(type(fuel_type))
+
+power_steering=True
+print(type(power_steering))
+
+tv=False
+print(type(tv))
