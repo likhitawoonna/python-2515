dfaaf5c975a2817e5a9bb599c5e7156d1de3ed82
likhitawoona <likhitawoona@gmail.com>
2025-07-07 18:31:27 +0530

    variablenotes

--- variables.py
+++ variables.py
@@ -1,3 +1,29 @@
 #variables are names of memory location,which are used to store data
 a=10
 print(type(a))
+
+'''Variables allow you to
+
+            -> Manipulate Data
+            -> Access Data
+            -> Hold Data'''
+ '''-> id function : prints the memory address
+
+        -> type function : prints the datatype being assigned
+
+        -> Outputs
+
+            -> print - to Output values
+
+            -> Concatenation : Combining two more more strings
+                -> + (when used with strings) 
+                -> + (when used with numbers - addition operator) 
+
+        -> Naming conventions
+
+            # ProductName --> Pascal Case (ClassNames)
+
+            # productName --> CamelCase (objectNames)
+
+            # product_name --> SnakeCase (When using variables & functions)'''
+
